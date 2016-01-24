c607599cfa2dc114c53aaf38f7f6bd4c7c8ee421
Lu Li <luli@Lus-MacBook-Pro.sv.cmu.local>
2016-01-23 22:14:14 -0800

    add sender

--- lab0/src/main/java/lab0/Sender.java
+++ lab0/src/main/java/lab0/Sender.java
@@ -6,39 +6,48 @@ import java.util.HashMap;
 
 public class Sender implements Runnable {
 	// <name, stream>
-	private HashMap<String, StreamPair> map;
+	private HashMap<String, StreamPair> stream;
 	// <name, info>
 	private HashMap<String, ServerInfo> info;
 	
-	private int serverPort;
-	
-	
-	public Sender(int serverPort) {
-		this.serverPort = serverPort;
-	}
+//	private int serverPort;
+//	
+//	
+//	public Sender(int serverPort) {
+//		this.serverPort = serverPort;
+//	}
 
 
 
 	public void run() {
 		while (true) {
 			try {
+				// TODO: change to get this message from queue
 				Message message = new Message("10.0.22.146", "kind", "first out msg");
                 String serverName = message.getDest();
-                String serverIp = info.get(serverName).getIp();
-                int serverPort = info.get(serverName).getPort();
-				// TODO: change to find stream from stored map first, if none, open a new socket
-				Socket socket = new Socket(serverIp, serverPort);
-//				Socket socket = new Socket("10.0.22.146", serverPort);
+                // first get outputstream from server name
+                StreamPair sp = stream.get(serverName);
+                ObjectOutputStream oos = sp.getOos();
+                ObjectInputStream ois = sp.getOis();
+                // if outputStream not exist, new one socket
+                if (oos == null) {
+                	String serverIp = info.get(serverName).getIp();
+                    int serverPort = info.get(serverName).getPort();
+    				Socket socket = new Socket(serverIp, serverPort);
+    				oos = new ObjectOutputStream(socket.getOutputStream());
+    				ois = new ObjectInputStream(socket.getInputStream());
+    				Message firstRep = (Message)ois.readObject();
+    				System.out.println("first response from new connection : " + firstRep.getData());
+    				StreamPair newpair = new StreamPair(ois, oos);
+    				stream.put(serverName, newpair);
+    				
+    				// Create a new thread to receive message from this new connection.
+    				Thread thread = new Thread(new Receiver(ois));
+                    thread.start();
+                }
 				System.out.println("sender starts");
-				// TODO: use map to get stream if exist, otherwise new one
-				ObjectOutputStream oos = new ObjectOutputStream(socket.getOutputStream());
 				oos.writeObject(message);
-				ObjectInputStream ois = new ObjectInputStream(socket.getInputStream());
-				ois.readObject();
 				
-				// Create a new thread to receive message from this new connection.
-				Thread thread = new Thread(new Receiver(ois));
-                thread.start();
 			} catch (IOException e) {
 				e.printStackTrace();
 			} catch (ClassNotFoundException e) {
