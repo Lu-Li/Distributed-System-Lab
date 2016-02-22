b0aca0044e4c0d96c27dda78199cac0e2c559fe7
baiqizhang <baiqiz@andrew.cmu.edu>
2016-02-21 23:29:38 -0800

    add more & organized logs

--- Lab0/src/application/Locker.java
+++ Lab0/src/application/Locker.java
@@ -13,13 +13,13 @@ public class Locker implements DistributedApplication{
 		hold,release,want
 	};
 	
-	static String myGroup; //TODO: grace read my group info
+	static String myGroup; 		//TODO: grace read my group info
 	static int myGroupSize = 3; //TODO: grace read my group info
 	
-	static Queue<String> queue = new LinkedList<>(); // queue of src
-	static State state = State.release; // current state
-	static int counter = 0; // ack counter
-	static boolean voting = false; // voting or not
+	static Queue<String> queue = new LinkedList<>(); 	// queue of src
+	static State state = State.release; 				// current state
+	static int counter = 0; 		// ack counter
+	static boolean voting = false; 	// voting or not
 	
 	static boolean enabled = false; //used to be compatible with existing code
 	
@@ -51,31 +51,33 @@ public class Locker implements DistributedApplication{
 	 * functionalities
 	 */
 	public static void onEvent(String eventType, String src){
-		if (eventType.equals("request")){
-			Log.info("Locker", "request received");
-			
+		if (eventType.equals("request")){			
 			if (state == State.hold || voting){
 				// cannot vote, wait
 				queue.add(src);
+				Log.info("Locker", "request received, cannot vote, waiting.. qsize = "+queue.size());	
 			} else {
+				Log.info("Locker", "request received, vote for "+src);
+				
 				//vote for that request's src
 				Message message = new Message(src, "ack", "no payload");
 				MessagePasser.send(message);
 				voting = true;
 			}
 		} else if (eventType.equals("release")){
-			Log.info("Locker", "release received");
-			
 			if (queue.size()!=0){
 				String waitingReq = queue.poll();
 				Message message = new Message(waitingReq, "ack", "no payload");
+				
+				Log.info("Locker", "release received, send ack to first waiting requester:"+waitingReq);
 				MessagePasser.send(message);
 				voting = true;				
 			} else {
+				Log.info("Locker", "release received, no one waiting, voting = false");
 				voting = false;
 			}
 		} else if (eventType.equals("ack")){
-			Log.info("Locker", "ack received, now = "+counter);
+			Log.info("Locker", "ack received, counter = "+counter);
 			
 			counter++;
 			if (counter == myGroupSize){
@@ -94,6 +96,8 @@ public class Locker implements DistributedApplication{
 			return;
 		}
 		state = State.want;		
+		Log.verbose("Locker", "sending request to all in group "+myGroup);
+
 		//multicast to all member in my group, wait for acks
 		counter = 0;
 		multiCaster.R_MultiCast(myGroup, new Message(null, "request", "no payload"));
@@ -107,7 +111,9 @@ public class Locker implements DistributedApplication{
 			System.out.println("Not in CS");
 			return;
 		}		
+		Log.verbose("Locker", "sending release to all in group "+myGroup);
 		state = State.release;
+		
 		//multicast to all member in my group
 		multiCaster.R_MultiCast(myGroup, new Message(null, "release", "no payload"));
 	}
